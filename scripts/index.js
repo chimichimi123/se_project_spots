ab5df066472665f359a225ecb9a17c470d643b38
Chimi <chriscantuwork580@gmail.com>
2025-04-22 10:47:30 -0500

    review stage 3

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,63 @@
-const editProfileBtn = document.querySelector("profile__edit-btn");
+const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
+const editProfileForm = editProfileModal.querySelector(".modal__form");
 
 const newPostBtn = document.querySelector(".profile__new-post-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__desc");
+
+function openModal(modal) {
+  modal.classList.add("modal_is-opened");
+  requestAnimationFrame(() => {
+    modal.classList.add("modal_visible");
+  });
+}
+
+function closeModal(modal) {
+  modal.classList.remove("modal_visible");
+  modal.addEventListener(
+    "transitionend",
+    () => {
+      modal.classList.remove("modal_is-opened");
+    },
+    { once: true }
+  );
+}
+
 editProfileBtn.addEventListener("click", function () {
-  editProfileModal.classList.add("modal_is-opened");
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
+  openModal(editProfileModal);
 });
 
 editProfileCloseBtn.addEventListener("click", function () {
-  editProfileModal.classList.remove("modal_is-opened");
+  closeModal(editProfileModal);
 });
 
 newPostBtn.addEventListener("click", function () {
-  editProfileModal.classList.add("modal_is-opened");
+  openModal(newPostModal);
 });
 
 newPostCloseBtn.addEventListener("click", function () {
-  editProfileModal.classList.remove("modal_is-opened");
+  closeModal(newPostModal);
 });
+
+function handleEditProfileSubmit(evt) {
+  evt.preventDefault();
+  profileNameEl.textContent = editProfileNameInput.value;
+  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+  console.log(profileNameEl.textContent, profileDescriptionEl.textContent);
+  closeModal(editProfileModal);
+}
+
+editProfileForm.addEventListener("submit", handleEditProfileSubmit);
